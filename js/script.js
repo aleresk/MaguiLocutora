c3557de328d67d2a0b34bb2ef8a2e3d63dc1e0db
aleresk <alejandro.resk@outlook.com>
2021-09-08 19:21:12 -0300

    final

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+    autoplay: true,
+    nav: true,
+    loop: true,
+    responsive:{
+        0:{
+            items: 1,
+        },
+        485:{
+            items: 2,
+        },
+        760:{
+            items: 3,
+        }
+    }
+
+});
